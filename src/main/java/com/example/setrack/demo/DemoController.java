366177a0ca91592ed05896ffd745c9bac9262d2a
brightboost <Ironman1@>
2019-01-16 15:34:31 +0100

    post working

--- src/main/java/com/example/setrack/demo/DemoController.java
+++ src/main/java/com/example/setrack/demo/DemoController.java
@@ -2,6 +2,7 @@ package com.example.setrack.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +16,11 @@ public class DemoController {
     @Autowired
     private DemoRepository demoRepository;
 
+
     @RequestMapping(value = "/get-demos", method = RequestMethod.GET)
     public List<Demo> getAll(){
+        //response entity
+
         return demoRepository.findAll();
     }
 
@@ -25,11 +29,10 @@ public class DemoController {
         return demoRepository.findById(id);
     }
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public void saveDemo(@PathVariable("name") String name){
-        Demo d = new Demo();
-        d.setName(name);
-        demoRepository.save(d);
+    @RequestMapping(value = "/save", method = RequestMethod.POST, produces="application/json", consumes="application/json")
+    public void saveDemo(@RequestBody Demo demo){
+        System.out.println(demo.getName());
+        demoRepository.save(demo);
 
         //demoRepository.save(demo);
     }
